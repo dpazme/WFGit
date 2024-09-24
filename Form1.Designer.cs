60e3373b13f515f6403a658898bc8c7f6e0ddfcd
damdepm <damdepm@gmail.com>
2024-09-24 10:27:08 +0200

    se añade segundo boton

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.butAceptar = new System.Windows.Forms.Button();
+            this.butsegundo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // butAceptar
@@ -40,11 +41,21 @@
             this.butAceptar.Text = "aceptar";
             this.butAceptar.UseVisualStyleBackColor = true;
             // 
+            // butsegundo
+            // 
+            this.butsegundo.Location = new System.Drawing.Point(483, 245);
+            this.butsegundo.Name = "butsegundo";
+            this.butsegundo.Size = new System.Drawing.Size(75, 23);
+            this.butsegundo.TabIndex = 1;
+            this.butsegundo.Text = "segundo";
+            this.butsegundo.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butsegundo);
             this.Controls.Add(this.butAceptar);
             this.Name = "Form1";
             this.Text = "FORMULARIOejemModificado";
@@ -55,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.Button butAceptar;
+        private System.Windows.Forms.Button butsegundo;
     }
 }
 
